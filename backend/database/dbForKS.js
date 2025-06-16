6b893dc1ba21a980b3cd3c64ec6afeb11b0ce964
ibnusina1234 <196341369+ibnusina1234@users.noreply.github.com>
2025-06-16 15:26:15 +0700

    unconnected localhost

--- backend/database/dbForKS.js
+++ backend/database/dbForKS.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql2');
-require("dotenv").config();
+// const mysql = require('mysql2');
+// require("dotenv").config();
 
-// Create a pool + promise
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10, // Bebas, 5-20 biasanya cukup
-  queueLimit: 0,
-}).promise(); // <== ini penting!
+// // Create a pool + promise
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10, // Bebas, 5-20 biasanya cukup
+//   queueLimit: 0,
+// }).promise(); // <== ini penting!
 
-module.exports = pool;
+// module.exports = pool;
--- backend/database/index.js
+++ backend/database/index.js
@@ -1,31 +1,31 @@
-// db.js
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+// // db.js
+// const mysql = require('mysql2/promise');
+// const dotenv = require('dotenv');
 
-// Load environment variables
-dotenv.config();
+// // Load environment variables
+// dotenv.config();
 
-// Buat koneksi pool
-const db = mysql.createPool({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASS ,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// // Buat koneksi pool
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST, 
+//   user: process.env.DB_USER ,
+//   password: process.env.DB_PASS ,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
-// Tes koneksi saat startup
-(async () => {
-  try {
-    const connection = await db.getConnection();
-    console.log('[DB] MySQL Pool Connected Successfully');
-    connection.release();
-  } catch (error) {
-    console.error('[DB] MySQL Pool Connection Error:', error.message);
-    process.exit(1); // Exit server jika gagal konek DB
-  }
-})();
+// // Tes koneksi saat startup
+// (async () => {
+//   try {
+//     const connection = await db.getConnection();
+//     console.log('[DB] MySQL Pool Connected Successfully');
+//     connection.release();
+//   } catch (error) {
+//     console.error('[DB] MySQL Pool Connection Error:', error.message);
+//     process.exit(1); // Exit server jika gagal konek DB
+//   }
+// })();
 
-module.exports = db;
+// module.exports = db;
