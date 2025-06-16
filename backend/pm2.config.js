ff71b6cf9378412603f5dfc6ba5bee642d1a74fb
ibnusina1234 <196341369+ibnusina1234@users.noreply.github.com>
2025-06-16 15:33:36 +0700

    update pm2.confight

--- backend/pm2.config.js
+++ backend/pm2.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: "qc", // Ganti dengan nama aplikasi Anda
-        script: "index.js", // Ganti dengan nama file utama backend Anda
-        watch: true,
-        ignore_watch: ["node_modules"],
-        autorestart: true,
-        cron_restart: "0 */1 * * *", // Setiap 1 jam
-        env: {
-          NODE_ENV: "production",
-        },
+  apps: [
+    {
+      name: "qc", // Ganti dengan nama aplikasi Anda
+      script: "server.js", // Ganti dengan nama file utama backend Anda
+      watch: true,
+      ignore_watch: ["node_modules"],
+      autorestart: true,
+      cron_restart: "0 */1 * * *", // Setiap 1 jam
+      env: {
+        NODE_ENV: "production",
       },
-    ],
-  };
-  
+    },
+  ],
+};
