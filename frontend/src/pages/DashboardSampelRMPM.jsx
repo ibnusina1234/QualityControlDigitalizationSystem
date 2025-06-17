71329eb5f395a21af3b3d53c35f2578d7e4e05f8
ibnusina1234 <196341369+ibnusina1234@users.noreply.github.com>
2025-06-17 13:57:43 +0700

    update DashboardRMPM.jsx bagian API

--- frontend/src/pages/DashboardSampelRMPM.jsx
+++ frontend/src/pages/DashboardSampelRMPM.jsx
@@ -172,7 +172,8 @@ export default function SampleMonitoringDashboard() {
             setLoading(true);
             try {
                   // Replace the URL below with your real API endpoint!
-                  const response = await fetch("http://localhost:8081/dashboardRMPM/dashboardRMPM-data"); // <-- Ganti ke endpoint backend kamu
+               const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/dashboardRMPM/dashboardRMPM-data`);
+ // <-- Ganti ke endpoint backend kamu
                   if (!response.ok) throw new Error("Failed to fetch data");
                   const data = await response.json();
                   setQcData(data);
